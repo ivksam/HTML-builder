1288adb44226a7e02fcaec26997cbd412bd1551a
ivksam <ivksam@ukr.net>
2022-11-04 16:37:53 +0200

    feat: add solution to the task 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,39 @@
+const fs = require('fs');
+const path = require('path');
+const sourceFolder = 'files';
+const targetFolder = 'files-copy';
 
+fs.mkdir(path.join(__dirname, targetFolder), {
+    recursive: true}, error => {
+    if (error) {
+        console.log(error.message)
+    };
+})
+
+fs.readdir(path.join(__dirname, targetFolder), {
+    withFileTypes: true}, (error, files) => {
+    if (error) {
+        console.log(error.message)
+    };
+
+    files.forEach(file => {
+        fs.unlink(path.join(__dirname, targetFolder, file.name), (error) => {
+            if (error) {
+                console.log(error.message)
+            };
+        });
+    });
+});
+
+fs.readdir(path.join(__dirname, sourceFolder), { withFileTypes: true }, (error, files) => {
+    if (error) {
+        console.log(error.message)
+    };
+    files.forEach(file => {
+        fs.copyFile(path.join(__dirname, sourceFolder, file.name), path.join(__dirname, targetFolder, file.name), (error) => {
+            if (error) {
+                console.log(error.message)
+            };
+        });
+    });
+});
